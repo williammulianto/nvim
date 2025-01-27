5c0cc55111f7914fd863a62e7608e3c60d7fba9d
William Mulianto <36563652+williammulianto@users.noreply.github.com>
2025-01-27 20:00:40 +0800

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "shaunsingh/nord.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "Mofiqul/dracula.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
